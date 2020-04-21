20d1e01a3efe2b756a4100fd2f50421922a04c59
VinceGHER <vincekillerz@gmail.com>
2020-04-21 21:29:21 +0200

    ajout de graphic_color

---
+++ graphic_color.h
@@ -0,0 +1,10 @@
+// Module Node (interface)
+// made by Vincent GHEROLD and Emile CAILLOL
+// version 1.0 
+
+#ifndef GRAPHIC_COLOR_H
+#define GRAPHIC_COLOR_H
+
+enum Color {BLACK,RED,GREEN};
+
+#endif
